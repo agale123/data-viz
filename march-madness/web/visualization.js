66c06ed76d2a08dd23a0cf5d8994c3650e2ca273
Alison Gale <agale@chromium.org>
2023-03-27 16:41:05 -0700

    Figure out layout

--- march-madness/web/visualization.js
+++ march-madness/web/visualization.js
@@ -1,8 +1,7 @@
 // Layout constants
 let width = 1000;
+let height = 200;
 const MAX_WIDTH = 1000;
-const HEIGHT = 120;
-const COLUMNS = [0.8, 0.8, 0.9, 1, 1.1, 1.1, 1.1];
 
 // Highlight constants
 let seed = undefined;
@@ -38,14 +37,13 @@ function opacityTeam(t) {
 function calcRadius(finish) {
     // Ensure it is an integer
     finish = parseInt(finish);
-    return (width / 30) * (1 - Math.log2(2 * finish) / Math.log2(2 * 2 * 64));
+    return height / 7 * (1 - Math.log2(2 * finish) / Math.log2(2 * 2 * 64));
 }
 
 /** Returns a deduplicated and sorted version of a list. */
 function dedupeAndSort(list) {
     const set = new Set(list);
     if (typeof set.values().next().value === "string") {
-        console.log("string");
         return [...set.values()].sort();
     } else {
         return [...set.values()].sort((a, b) => a - b);
@@ -54,16 +52,17 @@ function dedupeAndSort(list) {
 
 function toxy(r, angle) {
     return {
-        "x": 50 + r * Math.sin(Math.PI * 2 * angle / 360),
-        "y": 50 + r * Math.cos(Math.PI * 2 * angle / 360)
+        "x": height / 2 + r * Math.sin(Math.PI * 2 * angle / 360),
+        "y": height / 2 + r * Math.cos(Math.PI * 2 * angle / 360)
     };
 }
 
 function arrangeCircle(teams, circle_r, r) {
+    const h = height / 160;
     for (let i = 0; i < teams.length; i++) {
         teams[i] = {
             ...teams[i],
-            ...toxy(circle_r, 360 * i / teams.length),
+            ...toxy(circle_r * h, 360 * i / teams.length),
             "r": r
         };
     }
@@ -80,13 +79,13 @@ function getLocations(teams) {
         case 4:
             return arrangeCircle(teams, 25, radius);
         case 8:
-            const a = radius < 5 ? 0.4 : 1;
+            const a = teams[0]["finish"] === "64" ? 0.4 : 1;
             return [
                 ...arrangeCircle(teams.slice(0, 1), a * 0, radius),
                 ...arrangeCircle(teams.slice(1), a * 30, radius),
             ];
         case 16:
-            const b = radius < 5 ? 0.55 : 1;
+            const b = teams[0]["finish"] === "64" ? 0.55 : 1;
             return [
                 ...arrangeCircle(teams.slice(0, 1), b * 0, radius),
                 ...arrangeCircle(teams.slice(1, 6), b * 18, radius),
@@ -106,16 +105,9 @@ function getLocations(teams) {
 
 function drawPack(svg, x, y, teams) {
     const container = svg.append("g");
-    const root = d3.pack()
-        .size([100, 100])
-        .padding(3)
-        .radius(() => calcRadius(teams[0]["finish"]))
-        (d3.hierarchy({ children: teams }).sum(i => calcRadius(i["finish"])));
-
     const locations = getLocations(teams);
 
     container.selectAll("circle")
-        //.data(root.leaves())
         .data(locations)
         .join("circle")
         .attr("stroke", "none")
@@ -130,40 +122,43 @@ function drawYear(teams, year) {
     const subset = teams
         .filter(m => m["year"] === String(year) && m["finish"] !== "68");
 
+    // Calculate width for chart
     width = Math.min(document.querySelector("#host").clientWidth, MAX_WIDTH);
+    height = 50 + 100 * (width / MAX_WIDTH)
 
     // Draw svg
     const svg = d3
         .select("#host")
         .append("svg")
         .attr("width", width)
-        .attr("height", HEIGHT);
+        .attr("height", height);
 
     // Season title
     svg.append("g").attr("class", "text").append("text")
-        .attr("width", HEIGHT)
+        .attr("width", height)
         .attr("x", 20)
-        .attr("y", HEIGHT * 0.5)
+        .attr("y", height * 0.5)
         .attr("text-anchor", "middle")
-        .attr("transform", `rotate(270,20,${HEIGHT * 0.5})`)
+        .attr("transform", `rotate(270,20,${height * 0.5})`)
         .text(year);
 
     // Get finishes
     const finishes = dedupeAndSort(subset.map(m => parseInt(m["finish"])));
-    const denom = COLUMNS.reduce((a, b) => a + b, 0);
-    let x = 0.3 / denom * width;
+    let x = 10;
     for (let i = 0; i < finishes.length; i++) {
         const finish = finishes[i];
         const finishTeams = subset.filter(m => m["finish"] === `${finish}`);
-        drawPack(svg, x, HEIGHT / 2 - 50, finishTeams);
-
-        x = x + COLUMNS[i] / denom * width;
+        drawPack(svg, x, 0, finishTeams);
+        x += (i < 2 ? 0.8 : (i > 2 ? 1.2 : 1.1)) / 7.5 * (width - 20);
     }
 }
 
 let teams;
 
 async function drawChart() {
+    // Clear any past svg elements
+    d3.select("#host").selectAll("*").remove();
+
     const years = dedupeAndSort(teams.map(t => parseInt(t["year"]))).reverse();
     for (const year of years) {
         drawYear(teams, year);
@@ -185,6 +180,14 @@ async function initialize() {
         option.value = seed;
         seedSelect.add(option)
     }
+    seedSelect.addEventListener("change", event => {
+        if (event.target.value === "All") {
+            seed = undefined;
+        } else {
+            seed = parseInt(event.target.value);
+        }
+        drawChart();
+    });
 
     const teamSelect = document.getElementById("team");
     const sorted = dedupeAndSort(teams.map(t => t["team"]));
@@ -197,6 +200,19 @@ async function initialize() {
         option.value = team;
         teamSelect.add(option)
     }
+    teamSelect.addEventListener("change", event => {
+        if (event.target.value === "All") {
+            team = undefined;
+        } else {
+            team = event.target.value;
+        }
+        drawChart();
+    });
+
+    //Resize the d3 charts on a page resize
+    window.addEventListener("resize", () => {
+        drawChart();
+    });
 }
 
 initialize();
